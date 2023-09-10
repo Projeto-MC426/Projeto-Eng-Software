e6da33763bf5612e18bb5e62d94a2edf27d0f4ae
Gabriel Medrano Silva <g247113@dac.unicamp.br>
2023-09-10 19:31:44 -0300

    Adiciona uma opcao de login/logout. Ainda nao tem como criar um usuario

--- GymBuddy/base/urls.py
+++ GymBuddy/base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.paginaLogin, name='paginaLogin'),
+    path('logout/', views.logoutUsuario, name='paginaLogout'),
+
 ]
--- GymBuddy/base/views.py
+++ GymBuddy/base/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def paginaLogin(request):
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        try:
+            user = User.objects.get(username = usuario)
+        except:
+            messages.error(request, 'Usuario não encontrado')
+        aut = authenticate(request, username=usuario, password=senha)
+        if aut is not None:
+            login(request, aut)
+            return redirect("home")
+        
+    context = {}
+    return render(request, 'criar-login.html', context)
+
+def logoutUsuario(request):
+    logout(request)
+    return redirect("home")
 
 def home(request):
     return render(request, 'home.html')
